71ac839431cfcd3d26f4148b4f6b14944d437708
qlifford <otienocee@gmail.comgit config --global user.nameqlofford>
2023-08-14 08:24:23 +0300

    new commit

---
+++ main.py
@@ -0,0 +1,13 @@
+def cliff():
+    shape = "Triangle"
+    woozy = input("Whats your name?: ")
+
+    print("             .")
+    print("            /|")
+    print("           / |")
+    print("          /  |")
+    print("         /___|")
+    print("Hello " + woozy.upper() + ",did you see my new " + shape + "?")
+
+
+cliff()
